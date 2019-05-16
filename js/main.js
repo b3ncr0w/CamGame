627e59438cfd06502af1987507ded92cfd5fdfb0
Mateusz <bencrow96@gmail.com>
2019-05-17 00:50:18 +0200

    Prepared to frame-by-frame processing

--- js/main.js
+++ js/main.js
@@ -1,22 +1,59 @@
-// Grab elements, create settings, etc.
-var video = document.getElementById('video');
+let ctx;
+let canvas;
+let width = window.innerWidth;
+let height = window.innerHeight;
+let vwidth;
+let vheight;
+let video;
+let x0;
+let y0;
+let flag = false;
 
-// Get access to the camera!
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-        //video.src = window.URL.createObjectURL(stream);
-        video.srcObject = stream;
-        video.play();
-    });
+function init() {
+	video = document.getElementById('video');
+	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+		navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+			video.srcObject = stream;
+		});
+	}
+	video.addEventListener( "loadedmetadata", function (e) {
+		vwidth = video.videoWidth;
+		vheight = video.videoHeight;
+		flag = true;
+	}, false );
+
+	canvas = document.getElementById('canvas');
+	canvas.width = width;
+	canvas.height = height;
+	ctx = canvas.getContext('2d');
+	canvas2 = document.createElement('canvas');
 }
 
-// Elements for taking the snapshot
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-var video = document.getElementById('video');
+function compute(data) {
+	for(var i = 0; i < data.length; i+=4) {
+        var r = data[i];
+        var g = data[i+1];
+        var b = data[i+2];
+        var brightness = (3*r+4*g+b)>>>3;
+        data[i] = brightness;
+        data[i+1] = brightness;
+        data[i+2] = brightness;
+    }
+}
 
-// Trigger photo take
-document.getElementById("snap").addEventListener("click", function() {
-  context.drawImage(video, 0, 0, 640, 480);
-});
+mainloop = function() {
+	window.requestAnimationFrame(mainloop);
+	x0 = (width - vwidth)/2;
+	y0 = (height - vheight)/2;
+	ctx.drawImage(video, x0, y0, vwidth, vheight);
+	if(flag){
+		let idata = ctx.getImageData(x0, y0, vwidth, vheight);
+		let data = idata.data;
+		compute(data);
+		idata.data = data;
+		ctx.putImageData(idata,x0,y0);
+	}
+}
+
+init();
+mainloop();

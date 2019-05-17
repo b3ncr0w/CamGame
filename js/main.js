d2e1ef250c2545dae3c9505ae9393e0aa24ec0cb
Mateusz <bencrow96@gmail.com>
2019-05-17 12:28:22 +0200

    getColor() setColor() added

--- js/main.js
+++ js/main.js
@@ -1,25 +1,29 @@
 let ctx;
 let canvas;
-let width = window.innerWidth;
-let height = window.innerHeight;
-let vwidth;
-let vheight;
+let width = window.innerWidth; //canvas width
+let height = window.innerHeight; //canvas height
 let video;
-let x0;
-let y0;
-let flag = false;
+let vwidth; //video width
+let vheight; //vido height
+let data; //pixels data
+
+let x0, y0; //left top corner of video on canvas
+let videoLoaded = false;
 
 function init() {
-	video = document.getElementById('video');
+	video = document.createElement('video');
 	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 		navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
 			video.srcObject = stream;
+			video.play();
 		});
 	}
 	video.addEventListener( "loadedmetadata", function (e) {
 		vwidth = video.videoWidth;
 		vheight = video.videoHeight;
-		flag = true;
+		x0 = (width - vwidth)/2;
+		y0 = (height - vheight)/2;
+		videoLoaded = true;
 	}, false );
 
 	canvas = document.getElementById('canvas');
@@ -29,26 +33,37 @@ function init() {
 	canvas2 = document.createElement('canvas');
 }
 
-function compute(data) {
-	for(var i = 0; i < data.length; i+=4) {
-        var r = data[i];
-        var g = data[i+1];
-        var b = data[i+2];
-        var brightness = (3*r+4*g+b)>>>3;
-        data[i] = brightness;
-        data[i+1] = brightness;
-        data[i+2] = brightness;
-    }
+function getColor(x,y) {
+	let index = (vwidth*y+x)*4;
+	return [ data[index], data[index+1], data[index+2] ];
+}
+
+function setColor(x,y,color) {
+	let index = (vwidth*y+x)*4;
+	data[index] = color[0];
+	data[index+1] = color[1];
+	data[index+2] = color[2];
+}
+
+function compute() {
+	for(let h=0; h<vheight; h++) {
+		for(let w=0; w<vwidth; w++) {
+			let r = getColor(w,h)[0]*0.2126;
+			let g = getColor(w,h)[1]*0.7152;
+			let b = getColor(w,h)[2]*0.0722;
+			let c = r+g+b;
+			setColor(w,h,[c,c,c]);
+		}
+	}
 }
 
 mainloop = function() {
 	window.requestAnimationFrame(mainloop);
-	x0 = (width - vwidth)/2;
-	y0 = (height - vheight)/2;
+	
 	ctx.drawImage(video, x0, y0, vwidth, vheight);
-	if(flag){
+	if(videoLoaded){
 		let idata = ctx.getImageData(x0, y0, vwidth, vheight);
-		let data = idata.data;
+		data = idata.data;
 		compute(data);
 		idata.data = data;
 		ctx.putImageData(idata,x0,y0);
